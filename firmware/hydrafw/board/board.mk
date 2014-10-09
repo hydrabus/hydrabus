71b59d45ad1ba4e44e7173c502723399e8dc3bfd
bvernoux <bvernoux@gmail.com>
2014-10-09 22:32:14 +0200

    directory hydrafw deleted/replaced by submodule hydrafw

--- firmware/hydrafw/board/board.mk
+++
@@ -1,5 +0,0 @@
-# List of all the board related files.
-BOARDSRC = ./board/board.c
-
-# Required include directories
-BOARDINC = ./board
--- firmware/hydrafw/hydrabus/hydrabus.mk
+++
@@ -1,5 +0,0 @@
-# List of all the hydrabus related files.
-HYDRABUSSRC = hydrabus/hydrabus.c \
-            hydrabus/hydrabus_microrl.c
-# Required include directories
-HYDRABUSINC = ./hydrabus
